f03a19c1cd74df73c390c058d9b4b1762885e3a8
sowilu <sima.simute1999@gmail.com>
2023-10-12 17:11:02 +0300

    Goal Sound

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -5,11 +5,18 @@ public class Goal : MonoBehaviour
 {
     public TextMeshProUGUI scoreTxt;
     int score = 0;
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Pin"))
         {
+            audioSource.Play();
             scoreTxt.text = (++score).ToString();
         }
     }
